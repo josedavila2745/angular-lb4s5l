aa387278ab8d3e3fa5a59150dfb4982128acf1e6
josedavila2745 <48841072+josedavila2745@users.noreply.github.com>
2023-02-27 16:32:11 -0500

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import 'zone.js/dist/zone';
-import { Component, OnInit,DoCheck } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { ObjectComponent } from './object/object.component';
