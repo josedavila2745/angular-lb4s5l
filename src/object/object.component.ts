6a26aab0f9ba1be465ff0cd75e78755fede129aa
josedavila2745 <48841072+josedavila2745@users.noreply.github.com>
2023-02-27 19:18:52 -0500

    1 file modified

--- src/object/object.component.ts
+++ src/object/object.component.ts
@@ -32,7 +32,7 @@ export class ObjectComponent {
   ngDoCheck() {
     //console.log("docheck: ", this.img, " hist: ", this.clicks);
     this.clicks=this.edo.getEstado();
-    long=this.clicks.length;
+    let long=this.clicks.length;
     if(long>1){
       if(this.clicks[long]==this.clicks[long-1] && this.clicks[long]==this.img ){
         this.nocolapsado=0;
