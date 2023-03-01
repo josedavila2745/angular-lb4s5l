4cf5c8dc8e0b56fa64b71441a536d57b8bbed248
josedavila2745 <48841072+josedavila2745@users.noreply.github.com>
2023-03-01 13:56:58 -0500

    2 files modified

--- src/object/object.component.ts
+++ src/object/object.component.ts
@@ -14,9 +14,9 @@ export class ObjectComponent {
   @Input() img!     : number;
   @Input() frente!  : string;
   @Output() clickado = new EventEmitter<string>();
-  dorso0:string = "https://firebasestorage.googleapis.com/v0/b/memoria-18394.appspot.com/o/imgs%2Fdorso2.jpg?alt=media&token=31b763f4-cfc5-4780-baae-9add3b291eb3";
+  dorso0 = "https://firebasestorage.googleapis.com/v0/b/memoria-18394.appspot.com/o/imgs%2Fdorso2.jpg?alt=media&token=31b763f4-cfc5-4780-baae-9add3b291eb3";
   frontal = this.frente;
-  dorso = (this.dorso1) ? this.frontal : this.dorso0;
+  dorso = "https://firebasestorage.googleapis.com/v0/b/memoria-18394.appspot.com/o/imgs%2Fdorso2.jpg?alt=media&token=31b763f4-cfc5-4780-baae-9add3b291eb3";
   nocolapsado = 1;
   endorso = 1;
   clicks:Edo[];
