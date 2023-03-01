f95e6abea788ec7668f4ed0cd17a80a284f4702b
josedavila2745 <48841072+josedavila2745@users.noreply.github.com>
2023-03-01 17:14:29 -0500

    3 files modified

--- src/object/object.component.ts
+++ src/object/object.component.ts
@@ -14,21 +14,23 @@ export class ObjectComponent implements OnInit{
   @Input() frente!  : string;
   @Input() dorso1!  : number;
   @Output() clickado = new EventEmitter<string>();
+
   dorso0 = "https://firebasestorage.googleapis.com/v0/b/memoria-18394.appspot.com/o/imgs%2Fdorso2.jpg?alt=media&token=31b763f4-cfc5-4780-baae-9add3b291eb3";
+  
   frontal = this.frente;
-  dorso!:string ;
+  dorso= this.dorso0;
   nocolapsado = 1;
   endorso = 1;
   clicks:Edo[];
   click1:Edo;
   click2:Edo;
   constructor(public edo: EstadoService) {
-    this.dorso = (this.dorso1) ? this.frontal : this.dorso0;
-    console.log(this.dorso)
+
   }
   ngOnInit(){
     
-    
+   // this.dorso = (this.dorso1) ? this.frontal : this.dorso0;
+    console.log(this.frente) 
 
   }
   fueClickado() {
@@ -37,7 +39,7 @@ export class ObjectComponent implements OnInit{
       //console.log("clicks: ", this.clicks); 
       let long=this.clicks.length;
       if(this.clicks[long-1].ido!=this.ido){
-        this.dorso=this.frontal;
+        this.dorso=this.frente;
         this.edo.setEstado({conteo:(this.clicks[long-1].conteo+1),img:this.img,ido:this.ido});
         //setTimeout(this.cambia, 500);
     }
