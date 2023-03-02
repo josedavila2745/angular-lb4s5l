a9000c210970a8e2863e4b4d1446c4e5cd59a9c8
josedavila2745 <48841072+josedavila2745@users.noreply.github.com>
2023-03-02 16:07:59 -0500

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -22,13 +22,14 @@ export class App implements OnInit, DoCheck{
   vals: number[]=[];
   vists:number[]=[];
   items:number[] = [];
+  vinit=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
   anterior=-1;  actual=-1;
   constructor(private vvistas: EstadoService) {
 
    }
    ngOnInit(){
     this.imgs=this.vvistas.getvVistas();
-    this.vvistas.setVistas([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+    this.vvistas.setVistas(this.vinit);
     this.vala=this.imgs.map((e,index)=>[index, Math.random()]);
     this.valo=this.vala.sort(function(a, b){return a[1] - b[1]});
     for (var i = 0; i < 8; i++) {
